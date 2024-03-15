86eb5375951538839c13c9a22e886801cb6cf9a5
Роман Воробель <roman.vorobel@lnu.edu.ua>
2024-03-15 10:49:37 +0200

    Initial Commit

---
+++ modul1/ArrayManipulator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace modul1
+{
+    public class ArrayManipulator
+    {
+        public static int[] GenerateRandomArray(int length, int min, int max)
+        {
+            int[] array = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                Random random = new Random();
+                array[i] = random.Next(min, max);
+            }
+
+            return array;
+        }
+
+        public static int FindMax(int[] array)
+        {
+            int max = array[0];
+            foreach (int item in array)
+            {
+                if (item > max)
+                {
+                    max = item;
+                }
+            }
+
+            return max;
+        }
+
+        public static void SortArray(int[] array)
+        {
+            Array.Sort(array);
+        }
+    }
+}
--- modul1/Program.cs
+++ modul1/Program.cs
@@ -1,10 +1,43 @@
 ﻿namespace modul1
 {
+    /*
+     Варіант 1:
+    Створіть клас ArrayManipulator, який має методи для роботи з масивами цілих чисел:
+    Метод GenerateRandomArray(int length, int min, int max), який створює та повертає новий масив заданої довжини з випадковими числами в діапазоні від min до max.
+    Метод FindMax(int[] array), який знаходить та повертає найбільший елемент у масиві.
+    Метод SortArray(int[] array), який сортує масив у зростаючому порядку.
+
+    Після створення класу запустіть програму, яка створює масив, знаходить найбільший елемент та сортує масив. Виведіть початковий масив, знайдений максимум та відсортований масив на консоль.
+     */
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            int[] array = ArrayManipulator.GenerateRandomArray(10, 0, 100);
+
+            Console.WriteLine("Task 1: Random array:");
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine("\n\nTask 2: Max value:");
+            int maxValue = ArrayManipulator.FindMax(array);
+            Console.WriteLine(maxValue);
+
+            Console.WriteLine("\nTask 3: Sorted array:");
+            ArrayManipulator.SortArray(array);
+            foreach (var item in array)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine("\n");
+        }
+
+        class Bla
+        {
+            public string Name { set; get; }
         }
     }
 }
